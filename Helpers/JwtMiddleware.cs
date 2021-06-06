8dead0c7dd830d286e33f4bc2b2bacbc3b844c75
Konrad <k.szpytma3@gmail.com>
2021-06-06 18:48:18 +0200

    Add JwtMiddleware

---
+++ Helpers/JwtMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using FalcoBackEnd.ModelsDTO;
+using FalcoBackEnd.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FalcoBackEnd.Helpers
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly AppSettingsDTO appSettings;
+
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettingsDTO> appSettings)
+        {
+            this.next = next;
+            this.appSettings = appSettings.Value;
+        }
+
+        public async Task Invoke(HttpContext context, ITokenService tokenService)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                attachUserToContext(context, tokenService, token);
+            }
+
+            await next(context);
+        }
+
+        private void attachUserToContext(HttpContext context, ITokenService tokenService, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var secretKey = Encoding.ASCII.GetBytes(appSettings.Secret);
+                tokenHandler.ValidateToken(token, new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                context.Items["User"] = userService.GetById(userId);
+
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+    }
+}
