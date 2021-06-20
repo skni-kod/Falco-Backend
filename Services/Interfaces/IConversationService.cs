60b2ac2877006cf9e8f95fd6c359c856eacc7847
Konrad <k.szpytma3@gmail.com>
2021-06-20 15:39:39 +0200

    Add conversation interface

---
+++ Services/Interfaces/IConversationService.cs
@@ -0,0 +1,14 @@
+﻿using FalcoBackEnd.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FalcoBackEnd.Services.Interfaces
+{
+    public interface IConversationService
+    {
+        ConversationDTO GetConversation(int conversationID);
+        ResponseDTO AddConversation(params int[] owners);
+    }
+}
