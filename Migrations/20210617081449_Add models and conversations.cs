cfb89ee47d54d844611b7d0c1deb5a9881a330a2
Konrad <k.szpytma3@gmail.com>
2021-09-16 15:23:07 +0200

    Updated git ignore

---
+++ Migrations/20210617081449_Add models and conversations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FalcoBackEnd.Migrations
+{
+    public partial class Addmodelsandconversations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conversations",
+                columns: table => new
+                {
+                    Converastion_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Owners = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conversations", x => x.Converastion_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Message_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Conversation_id = table.Column<int>(type: "int", nullable: false),
+                    Author_id = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConversationConverastion_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Message_id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Conversations_ConversationConverastion_id",
+                        column: x => x.ConversationConverastion_id,
+                        principalTable: "Conversations",
+                        principalColumn: "Converastion_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ConversationConverastion_id",
+                table: "Messages",
+                column: "ConversationConverastion_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Conversations");
+        }
+    }
+}
---
+++ Migrations/20210617083717_Add models and conversations2.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FalcoBackEnd.Migrations
+{
+    public partial class Addmodelsandconversations2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Conversations_ConversationConverastion_id",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ConversationConverastion_id",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "ConversationConverastion_id",
+                table: "Messages");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Conversation_id",
+                table: "Messages",
+                column: "Conversation_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Conversations_Conversation_id",
+                table: "Messages",
+                column: "Conversation_id",
+                principalTable: "Conversations",
+                principalColumn: "Converastion_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Conversations_Conversation_id",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_Conversation_id",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConversationConverastion_id",
+                table: "Messages",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ConversationConverastion_id",
+                table: "Messages",
+                column: "ConversationConverastion_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Conversations_ConversationConverastion_id",
+                table: "Messages",
+                column: "ConversationConverastion_id",
+                principalTable: "Conversations",
+                principalColumn: "Converastion_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
